372a68f7c2dbdadb6b84e651106e110837f358e0
Jennifer Cortez Gutierrez <jennifer.cortez@alvaco.com.mx>
2026-01-26 15:14:18 -0600

    Actualización del proyecto

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-<script>
 let index = 0;
 const slides = document.getElementById("slides");
-const total = slides.children.length;
 
-function moverCarrusel() {
-    index++;
-    if (index >= total) {
-        index = 0;
+if (slides) {
+    const total = slides.children.length;
+
+    function moverCarrusel() {
+        index = (index + 1) % total;
+        slides.style.transform = `translateX(-${index * 100}%)`;
     }
 
-    slides.style.transform = `translateX(-${index * 100}%)`;
+    setInterval(moverCarrusel, 3000); // cambia cada 3 segundos
 }
-
-setInterval(moverCarrusel, 3000); // cambia cada 3 segundos
-</script>
